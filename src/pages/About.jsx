19829b19d084b6d46d0967be556491b09e6c0d5a
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2024-11-03 20:13:04 +0530

    temp

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import TextScramble from '../utils/TextScramble';
+import React from "react"
+import TextScramble from "../utils/TextScramble"
 
 const About = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <TextScramble text="About Us" />
-    </div>
-  );
-};
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <TextScramble text="About Us" />
+        </div>
+    )
+}
 
-export default About;
+export default About
