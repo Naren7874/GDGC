6a73643854934f48905782cfe8c62dde8bfb93e7
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2024-11-04 18:00:01 +0530

    roll back to event

---
+++ src/components/useSidebar.js
@@ -0,0 +1,13 @@
+import { useContext, createContext } from "react";
+
+const SidebarContext = createContext(undefined);
+
+export const useSidebar = () => {
+    const context = useContext(SidebarContext);
+    if (!context) {
+        throw new Error("useSidebar must be used within a SidebarProvider");
+    }
+    return context;
+};
+
+export default SidebarContext;
