99b8c10fa199f66d37948f7c264c574932133b06
Narendra <malinarendra7874@gmail.com>
2024-11-12 23:23:44 +0530

    update the Scramble effct

--- src/utils/TextScramble.jsx
+++ src/utils/TextScramble.jsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect, useRef } from 'react';
+import { useInView } from 'framer-motion';
 
 const TextScramble = ({ text }) => {
   const [displayedText, setDisplayedText] = useState(text);
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let interval = null;
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false }); 
 
   const scrambleText = () => {
     let iteration = 0;
@@ -31,24 +34,21 @@ const TextScramble = ({ text }) => {
     }, 30);
   };
 
-  // Run scramble effect on mount
+  // Run scramble effect only when the component comes into view
   useEffect(() => {
-    scrambleText();
+    if (isInView) {
+      scrambleText();
+    }
     return () => clearInterval(interval);
-  }, []);
+  }, [isInView]);
 
   return (
-    <motion.div
+    <div
+      ref={ref}
       className="font-mono text-white text-center text-3xl sm:text-5xl md:text-6xl lg:text-8xl xl:text-[8vw] 2xl:text-[6vw] p-4 sm:p-6 md:p-8 lg:p-12 xl:p-16 whitespace-nowrap rounded-md sm:rounded-lg"
-      data-value={text}
-      onMouseOver={scrambleText} // Also keep this for hover effect
-      initial={{ scale: 0 }} // Start with scale 0
-      whileInView={{ scale: 1 }} // Animate to scale 1 when in view
-      transition={{ duration: 0.5, ease: "easeOut" }} // Transition settings
-      viewport={{ once: false }} // Trigger only once when in view
     >
       {displayedText}
-    </motion.div>
+    </div>
   );
 };
 
