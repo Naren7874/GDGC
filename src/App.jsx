d0acb6132a888d30a13a7926bf19af7b649c6a9f
Narendra <malinarendra7874@gmail.com>
2024-11-13 01:46:08 +0530

    add loading effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,50 @@
 // App.jsx
-import MouseGlowEffect from "./utils/MouseGlowEffect"
-import HomePage from "./pages/HomePage"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Team from "./pages/Team"
-import Events from "./pages/Events"
-import Flashback from "./pages/Flashback"
-import StickyNavbar from "./components/Navbar"
+import React, { Suspense } from 'react';
+import MouseGlowEffect from "./utils/MouseGlowEffect";
+import Loader from './components/loader';
+import StickyNavbar from "./components/Navbar";
+
+// Lazy load all page components
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const About = React.lazy(() => import("./pages/About"));
+const Contact = React.lazy(() => import("./pages/Contact"));
+const Team = React.lazy(() => import("./pages/Team"));
+const Events = React.lazy(() => import("./pages/Events"));
+const Flashback = React.lazy(() => import("./pages/Flashback"));
 
 function App() {
     return (
         <div className="relative overflow-hidden">
-            {/* Fixed mouse glow effect for background */}
+            <Suspense fallback={<Loader />}>
             <div className="fixed inset-0 z-0 pointer-events-none">
                 <MouseGlowEffect />
             </div>
-
-            {/* Page content with scrolling layout */}
-            <div className="relative z-10 flex flex-col items-center">
-                <div className="top-0 w-screen flex justify-center items-center">
-                    <StickyNavbar />
+                <div className="relative z-10 flex flex-col items-center">
+                    <div className="top-0 w-screen flex justify-center items-center">
+                        <StickyNavbar />
+                    </div>
+                    <section id="home">
+                        <HomePage />
+                    </section>
+                    <section id="about">
+                        <About />
+                    </section>
+                    <section id="team">
+                        <Team />
+                    </section>
+                    <section id="events">
+                        <Events />
+                    </section>
+                    <section id="flashback">
+                        <Flashback />
+                    </section>
+                    <section id="contact">
+                        <Contact />
+                    </section>
                 </div>
-                <section id="home">
-                    <HomePage />
-                </section>
-                <section id="about">
-                    <About />
-                </section>
-                <section id="team">
-                    <Team />
-                </section>
-                <section id="events">
-                    <Events />
-                </section>
-                <section id="flashback">
-                    <Flashback />
-                </section>
-                <section id="contact">
-                    <Contact />
-                </section>
-            </div>
+            </Suspense>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
