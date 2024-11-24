ccfea3a362dd1fb30042075319943127cd07d54c
Narendra <malinarendra7874@gmail.com>
2024-11-24 18:45:25 +0530

    change in contat and social media links

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,56 +7,77 @@ import { IconChevronRight } from "@tabler/icons-react";
 const Contact = () => {
     return (
         <>
-            <footer className="text-white ">
-                    <TextScramble  text="Contact Us" />
+            <footer className="text-white">
+                <TextScramble text="Contact Us" />
                 <div className="my-10 flex flex-col">
                     {/* Social Media Links */}
                     <div className="flex justify-center">
-                        <div className=" grid grid-cols-4 sm:flex sm:justify-center gap-6">
+                        <div className="grid grid-cols-4 sm:flex sm:justify-center gap-6">
                             {/* LinkedIn */}
-                            <div className="group flex items-center space-x-2 sm:space-x-4">
+                            <a
+                                href="https://www.linkedin.com/company/gdsc-bvm/mycompany/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="group flex items-center space-x-2 sm:space-x-4"
+                            >
                                 <div className="icon bg-white rounded-full w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center shadow-lg transition-all duration-500 ease-out group-hover:bg-[#0077b5]">
                                     <FaLinkedinIn className="text-xl sm:text-2xl text-black group-hover:text-white" />
                                 </div>
-                                {/* Hide hover text on mobile */}
                                 <span className="hidden sm:inline text-sm sm:text-lg font-medium text-[#0077b5] opacity-0 group-hover:opacity-100 transition-opacity duration-500">
                                     LinkedIn
                                 </span>
-                            </div>
+                            </a>
 
                             {/* Discord */}
-                            <div className="group flex items-center space-x-2 sm:space-x-4">
+                            <a
+                                href="https://discord.com/invite/23Smc95Bzj"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="group flex items-center space-x-2 sm:space-x-4"
+                            >
                                 <div className="icon bg-white rounded-full w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center shadow-lg transition-all duration-500 ease-out group-hover:bg-[#5865F2]">
                                     <BsDiscord className="text-xl sm:text-2xl text-black group-hover:text-white" />
                                 </div>
                                 <span className="hidden sm:inline text-sm sm:text-lg font-medium text-[#5865F2] opacity-0 group-hover:opacity-100 transition-opacity duration-500">
                                     Discord
                                 </span>
-                            </div>
+                            </a>
 
                             {/* Instagram */}
-                            <div className="group flex items-center space-x-2 sm:space-x-4">
+                            <a
+                                href="https://www.instagram.com/gdg_bvm?igsh=MXhwa29nbmY2cjQ4cA=="
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="group flex items-center space-x-2 sm:space-x-4"
+                            >
                                 <div className="icon bg-white rounded-full w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center shadow-lg transition-all duration-500 ease-out group-hover:bg-[#e1306c]">
                                     <FaInstagram className="text-xl sm:text-2xl text-black group-hover:text-white" />
                                 </div>
                                 <span className="hidden sm:inline text-sm sm:text-lg font-medium text-[#e1306c] opacity-0 group-hover:opacity-100 transition-opacity duration-500">
                                     Instagram
                                 </span>
-                            </div>
+                            </a>
 
                             {/* Twitter */}
-                            <div className="group flex items-center space-x-2 sm:space-x-4">
+                            <a
+                                href="https://x.com/gdgbvm"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="group flex items-center space-x-2 sm:space-x-4"
+                            >
                                 <div className="icon bg-white rounded-full w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center shadow-lg transition-all duration-500 ease-out group-hover:bg-[#000000]">
                                     <BsTwitterX className="text-xl sm:text-2xl text-black group-hover:text-white" />
                                 </div>
                                 <span className="hidden sm:inline text-sm sm:text-lg font-medium text-[#1da1f2] opacity-0 group-hover:opacity-100 transition-opacity duration-500">
                                     Twitter
                                 </span>
-                            </div>
+                            </a>
                         </div>
                     </div>
 
                     <div className="h-16 bg-transparent"></div>
+
+                    {/* Google Map */}
                     <div className="flex justify-center rounded-xl p-5 overflow-hidden">
                         <iframe
                             title="BVM Map"
@@ -66,105 +87,105 @@ const Contact = () => {
                             className="rounded-xl"
                         />
                     </div>
-                </div>
-                <div className="h-16 bg-transparent"></div>
-                <div className="container mx-auto flex flex-col sm:flex-row justify-center gap-12 sm:gap-24 items-start px-4">
-                    {/* Left Section - Logo and About */}
-                    <div className="flex flex-col">
-                        <div className="flex items-center mb-4">
-                            <img
-                                src="/logo.png"
-                                alt="GDGC Logo"
-                                className="w-10 h-10 sm:w-12 sm:h-12 mr-2 sm:mr-3"
-                            />
-                            <span className="font-bold text-lg sm:text-xl">
-                                Google Developer Groups
-                            </span>
+
+                    <div className="h-16 bg-transparent"></div>
+
+                    <div className="container mx-auto flex flex-col sm:flex-row justify-center gap-12 sm:gap-24 items-start px-4">
+                        {/* Left Section - Logo and About */}
+                        <div className="flex flex-col">
+                            <div className="flex items-center mb-4">
+                                <img
+                                    src="/logo.png"
+                                    alt="GDGC Logo"
+                                    className="w-10 h-10 sm:w-12 sm:h-12 mr-2 sm:mr-3"
+                                />
+                                <span className="font-bold text-lg sm:text-xl">
+                                    Google Developer Groups
+                                </span>
+                            </div>
+                            <div className="text-gray-400">
+                                <h4 className="font-semibold mb-2">About</h4>
+                                <p className="text-sm sm:text-base">
+                                    Helping students bridge the gap between
+                                    theory and practice.
+                                </p>
+                            </div>
                         </div>
-                        <div className="text-gray-400">
-                            <h4 className="font-semibold mb-2">About</h4>
-                            <p className="text-sm sm:text-base">
-                                Helping students bridge the gap between theory
-                                and practice.
-                            </p>
+
+                        {/* Middle Section - Links */}
+                        <div className="flex flex-col">
+                            <h4 className="font-semibold mb-4">Links</h4>
+                            <ul className="text-gray-400 space-y-2 text-sm sm:text-base">
+                                <li className="flex gap-1">
+                                    <IconChevronRight />
+                                    <a href="#home" className="hover:underline">
+                                        Home
+                                    </a>
+                                </li>
+                                <li className="flex gap-1">
+                                    <IconChevronRight />
+                                    <a href="#about" className="hover:underline">
+                                        About
+                                    </a>
+                                </li>
+
+                                <li className="flex gap-1">
+                                    <IconChevronRight />
+                                    <a href="#team" className="hover:underline">
+                                        Team
+                                    </a>
+                                </li>
+
+                                <li className="flex gap-1">
+                                    <IconChevronRight />
+                                    <a
+                                        href="#contact"
+                                        className="hover:underline"
+                                    >
+                                        Contact
+                                    </a>
+                                </li>
+
+                                <li className="flex gap-1">
+                                    <IconChevronRight />
+                                    <a
+                                        href="#flashback2023"
+                                        className="hover:underline"
+                                    >
+                                        Flashback 2023 | 2022
+                                    </a>
+                                </li>
+                            
+                            </ul>
+                        </div>
+
+                        {/* Right Section - Contact Info */}
+                        <div className="flex flex-col">
+                            <h4 className="font-semibold mb-4">Have Questions?</h4>
+                            <ul className="text-gray-400 space-y-2 text-sm sm:text-base">
+                                <li>
+                                    <span className="inline-block mr-2">📍</span>
+                                    Birla Vishvakarma Mahavidyalaya,
+                                    Vidyanagar, Anand.
+                                </li>
+                                <li>
+                                    <span className="inline-block mr-2">📧</span>
+                                    <a href="mailto:developerstudentclubs.bvm@gmail.com">
+                                        developerstudentclubs.bvm@gmail.com
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
 
-                    {/* Middle Section - Links */}
-                    <div className="flex flex-col">
-                        <h4 className="font-semibold mb-4">Links</h4>
-                        <ul className="text-gray-400 space-y-2 text-sm sm:text-base">
-                            <li className="flex gap-1">
-                                <IconChevronRight />
-                                <a href="/" className="hover:underline">
-                                    Home
-                                </a>
-                            </li>
-                            <li className="flex gap-1">
-                                <IconChevronRight />
-                                <a href="/about" className="hover:underline">
-                                    About
-                                </a>
-                            </li>
-
-                            <li className="flex gap-1">
-                                <IconChevronRight />
-                                <a href="/about" className="hover:underline">
-                                    Team
-                                </a>
-                            </li>
-
-                            <li className="flex gap-1">
-                                <IconChevronRight />
-                                <a href="/about" className="hover:underline">
-                                    Contact
-                                </a>
-                            </li>
-
-                            <li className="flex gap-1">
-                                <IconChevronRight />
-                                <p> Flashback</p>{" "}
-                                <a href="/about" className="hover:underline">
-                                    {" "}
-                                    2023
-                                </a>
-                                <a href="/about" className="hover:underline">
-                                    {" "}
-                                    2022
-                                </a>
-                                <a href="/about" className="hover:underline">
-                                    {" "}
-                                    2021
-                                </a>
-                            </li>
-                        </ul>
+                    {/* Bottom Section */}
+                    <div className="text-center text-gray-500 mt-8 px-4">
+                        <p className="text-sm sm:text-base">
+                            Copyright © 2024 All rights reserved | Designed and
+                            Created By Narendra Mali & Jaimin Detroja
+                        </p>
+                        <div className="h-8 bg-transparent"></div>
                     </div>
-
-                    {/* Right Section - Contact Info */}
-                    <div className="flex flex-col">
-                        <h4 className="font-semibold mb-4">Have Questions?</h4>
-                        <ul className="text-gray-400 space-y-2 text-sm sm:text-base">
-                            <li>
-                                <span className="inline-block mr-2">📍</span>
-                                Birla Vishvakarma Mahavidyalaya, Vidyanagar,
-                                Anand.
-                            </li>
-                            <li>
-                                <span className="inline-block mr-2">📧</span>
-                                <a href="mailto:developerstudentclubs.bvm@gmail.com">
-                                    developerstudentclubs.bvm@gmail.com
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                {/* Bottom Section */}
-                <div className="text-center text-gray-500 mt-8 px-4">
-                    <p className="text-sm sm:text-base">
-                        Copyright © 2024 All rights reserved | Designed and Created By Narendra Mali & Jaimin Detroja
-                    </p>
-                <div className="h-8 bg-transparent"></div>
                 </div>
             </footer>
         </>
